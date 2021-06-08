8f6757bea93d130986a7d385d44de81c5299adb0
Dan <up765064@myport.ac.uk>
2021-06-08 10:33:10 +0100

    plan_started_first_test_done

---
+++ main.js
@@ -0,0 +1,11 @@
+//Test 1: Dividing by itself being zero
+// function isItPrime(n){
+//     if (n%n === 0){
+//         console.log(`${n} divided by itself is prime`)
+//     }
+// }
+// isItPrime(19)
+
+function isItPrime(n){
+    
+}
