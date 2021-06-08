4c8b9aa03c5f5812461a4c1c70df6f87228a1013
Dan <up765064@myport.ac.uk>
2021-06-08 10:40:19 +0100

    can_calculate_how_many_factors

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Test 1: Dividing by itself being zero
+// Test 1: Dividing by itself being zero
 // function isItPrime(n){
 //     if (n%n === 0){
 //         console.log(`${n} divided by itself is prime`)
@@ -7,5 +7,15 @@
 // isItPrime(19)
 
 function isItPrime(n){
-    
+    let factors = 0;
+    for(i=0; i<=n; i++){
+        if(n%i === 0){
+            factors = factors+1
+            console.log(`${n} has ${factors} factors.`)
+        }
+    }
 }
+
+isItPrime(10)
+
+isItPrime(13)
